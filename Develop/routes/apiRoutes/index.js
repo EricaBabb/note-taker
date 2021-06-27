3e74ef7b9c39cce97469b316f487ff3bc0a9cc6a
EricaBabb <ecamilebabb@gmail.com>
2021-06-27 17:55:51 -0400

    revert

--- Develop/routes/apiRoutes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const notesRoute = require('../apiRoutes/notesRoute');
-
-router.use(notesRoute);
-router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-  });
-
-module.exports = router;
